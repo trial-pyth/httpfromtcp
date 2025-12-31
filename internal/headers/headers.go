dfaff9df0c635be6e60ff00847921120a36d0209
trial-pyth <smigr1279@gmail.com>
2025-12-31 09:47:20 +0000

    redid headers parse

---
+++ internal/headers/headers.go
@@ -0,0 +1,57 @@
+package headers
+
+import (
+	"bytes"
+	"fmt"
+)
+
+var rn = []byte("\r\n")
+
+type Headers map[string]string
+
+func NewHeaders() Headers {
+	return map[string]string{}
+}
+
+func parseHeader(fieldLine []byte) (string, string, error) {
+	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf("malformed field line")
+
+	}
+	name := parts[0]
+	value := bytes.TrimSpace(parts[1])
+
+	if bytes.HasSuffix(name, []byte(" ")) {
+		return "", "", fmt.Errorf("malformed field name")
+	}
+
+	return string(name), string(value), nil
+}
+
+func (h Headers) Parse(data []byte) (int, bool, error) {
+	read := 0
+	done := false
+	for {
+		idx := bytes.Index(data[read:], rn)
+		if idx == -1 {
+			break
+		}
+
+		if idx == 0 {
+			done = true
+			read += len(rn)
+			break
+		}
+
+		name, value, err := parseHeader(data[read:read+idx])
+		if err != nil {
+			return 0, false, err
+		}
+
+		read += idx + len(rn)
+		h[name] = value
+	}
+
+	return read, done, nil
+}
